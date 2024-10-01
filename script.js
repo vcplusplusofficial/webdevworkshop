7899431ba758387b879aa47f5793be23a6bae228
meerashroff <mshroff@vassar.edu>
2024-09-30 22:58:48 -0400

    added hamster (basic user input)

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ function changeColor() {
     button.innerText = "Clicked!"; // Change button text
   });
 }
+function garlicHamster() {
+  const hamster = document.getElementById("hamster");
+  const responsetext = document.getElementById("responsetext");
+  var response = "";
+  hamster.addEventListener("mouseover", function () {
+    response = prompt("what do you think of the garlic hamster?");
+    //prompt is one way of getting input from a user
+    //by saving the response in a variable, you can use that input later
+    responsetext.innerText = "you said " + response + " about the hamster";
+  });
+}
